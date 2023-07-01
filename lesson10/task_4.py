c17b08f4f5ed5e30b2e7882e4bc01b9ad90ee3a6
mystofoid <83120713+mystofoid@users.noreply.github.com>
2023-07-01 07:54:31 +0300

    lesson10 task_4.py done

---
+++ lesson10/task_4.py
@@ -0,0 +1,8 @@
+size = int(input('Введите размер матрицы: '))
+for row in range(1, size + 1):
+    for col in range(1, size + 1):
+        if row % 2 == 0:
+            print(row, end=' ')
+        else:
+            print(col, end=' ')
+    print()
