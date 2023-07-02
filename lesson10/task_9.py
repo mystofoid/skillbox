ec65209a9b1edbb33f5f775ba15a789318153e44
mystofoid <83120713+mystofoid@users.noreply.github.com>
2023-07-02 10:30:37 +0300

    lesson10 task_9.py added teacher's solution

--- lesson10/task_9.py
+++ lesson10/task_9.py
@@ -2,15 +2,36 @@
 # Напишите программу, которая получает на вход размер квадратной матрицы и выводит на экран по такому принципу диагональ из единиц с левого нижнего угла до верхнего правого, ниже диагонали — двойки, выше — нули.
 
 # Пример:
+# 0 0 0 0 0 0 0 1 
+# 0 0 0 0 0 0 1 2
+# 0 0 0 0 0 1 2 2
+# 0 0 0 0 1 2 2 2
+# 0 0 0 1 2 2 2 2
+# 0 0 1 2 2 2 2 2
+# 0 1 2 2 2 2 2 2
+# 1 2 2 2 2 2 2 2
 
-size = int(input('Введите размер матрицы: '))
-for row in range(size):
-    for col in range(size, 0, -1):
-        # print(col)
-        if row == col - 1:
-            print('1', end=' ')
-        elif row > col - 1:
-            print('2', end=' ')
+n = int(input("Введите размер матрицы: "))
+
+for y in range(n):
+    for x in range(n):
+        buf_x = (n - 1) - y  # вспомогательное число, которое будет уменьшаться от n-1 до 0
+        if buf_x > x:
+            print(0, end='\t')
+        elif buf_x == x:
+            print(1, end='\t')
         else:
-            print('0', end=' ')
+            print(2, end='\t')
     print()
+
+
+# size = int(input('Введите размер матрицы: '))
+# for row in range(size):
+#     for col in range(size, 0, -1):
+#         if row == col - 1:
+#             print('1', end=' ')
+#         elif row > col - 1:
+#             print('2', end=' ')
+#         else:
+#             print('0', end=' ')
+#     print()
