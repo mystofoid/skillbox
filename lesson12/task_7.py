2b0dad30dfcd70c4a168a3dd5b6629dfa9192b55
mystofoid <83120713+mystofoid@users.noreply.github.com>
2023-07-27 21:13:43 +0300

    lesson12 task_7.py done

--- lesson12/task_7.py
+++ lesson12/task_7.py
@@ -14,12 +14,34 @@
 
 # Усложнение: сделайте это без использования циклов.
 
+# 1
+# def calcTool(x, y):
+#     sumNumbers, countNumbers = 0, 0
+#     for i in range(x, y + 1):
+#         sumNumbers += i
+#         countNumbers += 1
+#     print(f'Сраднее: {round(sumNumbers / countNumbers, 2)}')
+
+# 2
+# def calcTool(x, y): # Без Цикла
+#     print(f'Среднее: {round((x + y) / 2, 2)}')
+
+# a = int(input('Введите a: '))
+# b = int(input('Введите b: '))
+
+# if a > b:
+#     a, b = b, a
+
+# calcTool(a, b)
+
+# 3
+
 def calcTool(x, y):
-    sumNumbers, countNumbers = 0, 0
-    for i in range(x, y + 1):
-        sumNumbers += i
-        countNumbers += 1
-    print(f'Сраднее: {round(sumNumbers / countNumbers, 2)}')
+    list_num = list(range(a, b + 1))
+    average = sum(list_num) / len(list_num)
+    print(f'Среднее: {average, list_num}')
+
+
 
 a = int(input('Введите a: '))
 b = int(input('Введите b: '))
