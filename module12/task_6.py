723b5084cdd9765cab2502ffcfad33439c7b8d1b
mystofoid <83120713+mystofoid@users.noreply.github.com>
2023-08-07 22:50:03 +0300

    module12 task_6.py done

---
+++ module12/task_6.py
@@ -0,0 +1,17 @@
+# Задача 6. НОД
+# Что нужно сделать
+# Напишите функцию, вычисляющую наибольший общий делитель двух чисел.
+
+def calc_largest_devider():
+    number_1 = int(input('Введите первое число: '))
+    number_2 = int(input('Введите второе число: '))
+    num_1 = number_1
+    num_2 = number_2
+    while number_1 != 0 and number_2 != 0:
+        if number_1 > number_2:
+            number_1 = number_1 % number_2
+        else:
+            number_2 = number_2 % number_1
+    print(f'НОД чисел {num_1} и {num_2} равен', number_1 + number_2)
+
+calc_largest_devider()
