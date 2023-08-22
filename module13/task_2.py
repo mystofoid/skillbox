9ec048bdadaf30c1851dfd439fe4b11d78171431
mystofoid <83120713+mystofoid@users.noreply.github.com>
2023-08-22 22:43:18 +0300

    module13 task_2.py edited

--- module13/task_2.py
+++ module13/task_2.py
@@ -1,10 +1,10 @@
-# def divide(a, b):
+# def divide(a, b): 
 #     if b == 0:
 #         return None
 #     return a / b
 
-def divide(a, b):
-    if b != 0:
+def divide(a, b): 
+    if b != 0: # ! (b ! = 0) сократили код не меняя логики
         return a / b
 
 print(divide(10, 0))
