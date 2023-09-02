1c21c7929a07e571ce6b6e63de44238c3d8c7512
mystofoid <83120713+mystofoid@users.noreply.github.com>
2023-09-02 13:12:59 +0300

    module13/task_2.py modified

--- module13/task_2.py
+++ module13/task_2.py
@@ -3,8 +3,19 @@
 #         return None
 #     return a / b
 
-def divide(a, b): 
-    if b != 0: # ! (b ! = 0) сократили код не меняя логики
-        return a / b
+# def divide(a, b): 
+#     if b != 0: # ! (b ! = 0) сократили код не меняя логики
+#         return a / b
 
-print(divide(10, 0))
+# print(divide(10, 0))
+
+from decimal import Decimal
+
+def divide_n_times(value, divader, times):
+    value = Decimal(value)
+    for i in range(times):
+        value /= divader
+    # return value
+    print(value)
+
+divide_n_times(12345, 10, 5)
